584ee6728967ce7d0422031fa615c4c5b632dead
Duc Phan <ducphanduy.agentp@gmail.com>
2020-12-17 15:47:03 +0700

    Add remove function

--- app.js
+++ app.js
@@ -5,15 +5,14 @@ Vue.component('item-action', {
     <b-button variant="danger" @click="removeItem(item)"><b-icon icon="trash-fill"></b-icon> Remove</b-button>
     </b-button-group>`,
     methods: {
-      editItem: function (item) {
-        alert("Editing " + item.item_name);
+      editItem: function(item) {
+        this.$root.editItem(item);
       },
-  
-      removeItem: function (item) {
-        alert("Removing " + item.item_name);
+      removeItem: function(item) {
+        this.$root.removeItem(item);
       }
     }
-})
+});
 
 var app = new Vue({
   el: '#app',
@@ -42,13 +41,54 @@ var app = new Vue({
           console.log(error);
         });
     },
+    addItem(payload) {
+      const path = "http://localhost:5000/addItem";
+      axios.post(path, payload)
+        .then((res) => {
+          this.getItems();
+        })
+        .catch((error) => {
+          console.log(error);
+          this.getItems();
+        });
+    },
+    editItem(item) {
+      alert("Editing " + item.item_name);
+    },
+    removeItem(item) {
+      const path = "http://localhost:5000/removeItem";
+      const payload = {
+        id: item.id
+      }
+      axios.post(path, payload)
+        .then((res) => {
+          this.getItems();
+        })
+        .catch((error) => {
+          console.log(error);
+          this.getItems();
+        });
+    },
+    initItemForm() {
+      this.addItemForm.item_name = '';
+      this.addItemForm.quantity = 0;
+      this.addItemForm.unit = '';
+    },
     submitAddBook: function(evt) {
       evt.preventDefault();
-      alert(1);
+      this.$refs.addItemModal.hide();
+      const payload = {
+        item_name: this.addItemForm.item_name,
+        quantity: this.addItemForm.quantity,
+        unit: this.addItemForm.unit
+      };
+      this.addItem(payload);
+      this.initItemForm();
     },
     resetAddBook: function(evt) {
       evt.preventDefault();
-      alert(2);
+      this.$refs.addItemModal.hide();
+      this.initItemForm();
     }
   },
   created: function () {
